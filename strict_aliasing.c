e904b672327c6c74e2cf743cb6eed9472b87984e
Rafal <gotchas.u@gmail.com>
2016-05-06 13:36:30 +0200

    add missing quiz_aliasing.cpp  strict_aliasing.c  tmpl_alias_overload.cpp

---
+++ strict_aliasing.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+ 
+long foo(int *x, long *y) {
+  *x = 0;
+  *y = 1;
+  return *x;
+}
+ 
+int main(void) {
+  long l;
+  printf("%ld\n", foo((int *)&l, &l));
+}
+
+/*
+
+#include <stdio.h>
+ 
+typedef struct { int i1; } s1;
+typedef struct { int i2; } s2;
+ 
+void f(s1 *s1p, s2 *s2p) {
+  s1p->i1 = 2;
+  s2p->i2 = 3;
+  printf("%i\n", s1p->i1);
+}
+ 
+int main() {
+  s1 s = {.i1 = 1};
+  f(&s, (s2 *)&s);
+}
+*/
+
