acd9cd6c48f8ededa9d83ce50efec1ece8992dae
Rafal <gotchas.u@gmail.com>
2016-05-06 13:23:45 +0200

    safer version for or operator for enums

---
+++ enum_bit_op.h
@@ -0,0 +1,38 @@
+#ifndef ENUM_BIT_OP_H
+#define ENUM_BIT_OP_H
+
+// http://accu.org/index.php/journals/2228
+
+#include  <type_traits>
+
+template<typename E>
+struct enable_bitmask_operators{
+    static constexpr bool enable=false;
+};
+
+template<typename E>
+typename
+  std::enable_if<enable_bitmask_operators<E>
+     ::enable,E>::type
+operator|(E lhs,E rhs) 
+{
+  typedef typename std::underlying_type<E>
+    ::type underlying;
+  return static_cast<E>(
+    static_cast<underlying>(lhs) 
+  | static_cast<underlying>(rhs));
+}
+
+#ifdef ENUM_BIT_OP_H_EXAMPLE
+enum class my_bitmask 
+{
+   first=1,second=2,third=4
+}:
+template<>
+struct enable_bitmask_operators<my_bitmask>{
+  static constexpr bool enable=true;
+};
+#endif
+
+#endif // ENUM_BIT_OP_H
+
---
+++ test_enum_bit_op.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "enum_bit_op.h"
+
+enum class my_bitmask
+{
+   first=1,second=2,third=4
+};
+
+template<>
+struct enable_bitmask_operators<my_bitmask>{
+  static constexpr bool enable=true;
+};
+
+int
+main()
+{
+  my_bitmask bm = my_bitmask::second | my_bitmask::third; 
+  std::cout << int(bm);
+  return 0;
+}
