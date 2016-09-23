f7b6c82c24ad0bd55aca2fa18a7696fbbaf51939
Rafal <rdzbek@gmail.com>
2016-09-23 13:21:01 +0200

    add src/17/constexpr_if.cpp

---
+++ src/17/constexpr_if.cpp
@@ -0,0 +1,15 @@
+// No main here! #define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+// https://www.youtube.com/watch?v=qHgM5UdzPQU
+// https://www.youtube.com/watch?v=_Ny6Qbm_uMI
+
+namespace constexpr_tests {
+
+// good for handling void return type in generic functions /but not always/
+BOOST_AUTO_TEST_SUITE(constexpr_tests)
+
+BOOST_AUTO_TEST_CASE(simple_test) {}
+
+BOOST_AUTO_TEST_SUITE_END()
+}
