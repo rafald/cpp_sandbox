b8891f7230fa5ee208e41013ea5292c4d09179dd
Rafal Rafalski <gotchas.u@gmail.com>
2017-05-08 11:27:06 +0200

    c++ version of ctci-ransom-note

---
+++ src/hr/ctci-ransom-note.cpp
@@ -0,0 +1,56 @@
+// clang++-4.0 -std=c++1z 
+
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+bool ransom_note(vector<string> magazine, vector<string> ransom) {
+    std::sort(magazine.begin(), magazine.end());
+    std::sort(ransom.begin(), ransom.end());
+    return includes(magazine.begin(), magazine.end(), ransom.begin(), ransom.end());
+}
+
+int main(int argn, char const * argv[]){
+    if(argn>1) {
+        static ifstream ifs{argv[1]};
+        cin.rdbuf(ifs.rdbuf());
+    }
+    int m;
+    int n;
+    cin >> m >> n;
+    vector<string> magazine(m);
+    for(int magazine_i = 0;magazine_i < m;magazine_i++){
+       cin >> magazine[magazine_i];
+    }
+    vector<string> ransom(n);
+    for(int ransom_i = 0;ransom_i < n;ransom_i++){
+       cin >> ransom[ransom_i];
+    }
+    if(ransom_note(magazine, ransom))
+        cout << "Yes\n";
+    else
+        cout << "No\n";
+    return 0;
+}
+
