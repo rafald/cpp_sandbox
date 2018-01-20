1026a75027b4f6c645f5f892777a66dc834d7735
Rafal Rafalski <gotchas.u@gmail.com>
2018-01-20 20:32:07 +0100

    src/cpp17tests.cpp add dummy auto unit test case to prevent error with empty list of tests

--- src/cpp17tests.cpp
+++ src/cpp17tests.cpp
@@ -3,3 +3,8 @@
 #include <boost/test/unit_test.hpp>
 
 
+BOOST_AUTO_TEST_CASE(dummy_test) {
+   // to overcome problems when test tree is empty because of weak compiler support for C++ standard
+}
+
+
