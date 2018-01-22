593a671b1f67bc58328855bacaae4498662fde46
Rafal Rafalski <gotchas.u@gmail.com>
2018-01-22 18:05:14 +0100

    build.sh: switch from bash to fish

--- build.fish
+++ build.fish
@@ -1,7 +1,9 @@
+#!/usr/bin/env fish
 #!/usr/bin/env bash
 
 echo
 echo ./build.sh --boost-root=/home/raf12/develop/c++/boost
 echo BOOST_ROOT=~/develop/c++/boost ./build.sh
 echo 
-bjam -j4 --dump-tests $@
+#bjam -j$nproc --dump-tests $@
+env toolset= bjam -j$nproc --dump-tests $argv
